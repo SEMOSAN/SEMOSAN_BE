e8a280fd27df4fe6a270593b870842fa477b46c1
Hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2026-05-11 01:16:11 +0900

    feat: Redis 의존성 및 설정 추가

---
+++ src/main/java/com/semosan/api/common/config/TrackingProperties.java
@@ -0,0 +1,16 @@
+package com.semosan.api.common.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "tracking")
+public class TrackingProperties {
+
+    private String streamKey;
+    private String consumerGroup;
+}
