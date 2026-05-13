95b6a17804c690bbb8a884b61447e589bd791a55
JangInho <wkddlsgh50@naver.com>
2026-05-13 20:40:18 +0900

    refactor: FreePostCreateRequest max dto 제약 검사 추가

--- src/main/java/com/semosan/api/domain/community/post/dto/FreePostCreateRequest.java
+++ src/main/java/com/semosan/api/domain/community/post/dto/FreePostCreateRequest.java
@@ -1,12 +1,13 @@
 package com.semosan.api.domain.community.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record FreePostCreateRequest(
-        @NotBlank String title,
-        @NotBlank String content,
+        @NotBlank @Size(max = 200) String title,
+        @NotBlank @Size(max = 5000) String content,
         List<String> imageUrls,
         Integer mainImageIndex
 ) {
