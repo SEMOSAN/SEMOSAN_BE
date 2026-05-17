74caf756177b10421f0d5f53eb1a9bd233923b16
pooreumjung <pooreumjung02@naver.com>
2026-05-17 20:38:09 +0900

    fix: HikingMemberRepository import 누락 에러 해결

--- src/main/java/com/semosan/api/domain/hiking/repository/HikingMemberRepository.java
+++ src/main/java/com/semosan/api/domain/hiking/repository/HikingMemberRepository.java
@@ -1,10 +1,17 @@
 package com.semosan.api.domain.hiking.repository;
 
 import com.semosan.api.domain.hiking.entity.HikingMember;
+import com.semosan.api.domain.hiking.entity.HikingRecord;
+import com.semosan.api.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+import java.util.List;
 
 public interface HikingMemberRepository extends JpaRepository<HikingMember, Long> {
 
