10d28d03641b94ef4983ffa8f8f1bcb6bfc45899
Hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2026-07-22 01:34:40 +0900

    fix: restaurant_sections 불필요 컬럼 삭제 마이그레이션

---
+++ src/main/resources/db/migration/V32__drop_restaurant_sections_unnecessary_columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE restaurant_sections DROP COLUMN menu;
+ALTER TABLE restaurant_sections DROP COLUMN description;
+ALTER TABLE restaurant_sections DROP COLUMN map_url;
+ALTER TABLE restaurant_sections DROP COLUMN blog_url;
