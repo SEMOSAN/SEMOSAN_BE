167b948dfacace38976dc7347b5cc9f5e75fdc54
pooreumjung <pooreumjung02@naver.com>
2026-08-06 16:25:29 +0900

    test: 알림 카카오 OAuth 테스트 보강

--- src/test/java/com/semosan/api/domain/oauth/client/OAuthKakaoClientTest.java
+++ src/test/java/com/semosan/api/domain/oauth/client/OAuthKakaoClientTest.java
@@ -6,6 +6,7 @@ import com.semosan.api.domain.oauth.dto.KakaoUserInfoResponse;
 import com.semosan.api.domain.oauth.properties.KakaoProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -57,6 +58,29 @@ class OAuthKakaoClientTest {
         when(headersSpec.retrieve()).thenReturn(responseSpec);
         when(responseSpec.onStatus(any(), any())).thenReturn(responseSpec);
         when(responseSpec.bodyToMono(KakaoUserInfoResponse.class)).thenReturn(Mono.empty());
+
+        assertThatThrownBy(() -> client.getKakaoUserInfo("access-token"))
+                .isInstanceOf(GeneralException.class)
+                .extracting("errorStatus")
+                .isEqualTo(ErrorStatus.KAKAO_USER_INFO_REQUEST_FAILED);
+    }
+
+    @Test
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    void getKakaoUserInfoThrowsWhenResponseIdIsNull() {
+        WebClient webClient = mock(WebClient.class);
+        WebClient.RequestHeadersUriSpec uriSpec = mock(WebClient.RequestHeadersUriSpec.class);
+        WebClient.RequestHeadersSpec headersSpec = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
+        OAuthKakaoClient client = new OAuthKakaoClient(webClient, kakaoProperties());
+
+        when(webClient.get()).thenReturn(uriSpec);
+        when(uriSpec.uri("/v2/user/me")).thenReturn(headersSpec);
+        when(headersSpec.header("Authorization", "Bearer access-token")).thenReturn(headersSpec);
+        when(headersSpec.retrieve()).thenReturn(responseSpec);
+        when(responseSpec.onStatus(any(), any())).thenReturn(responseSpec);
+        when(responseSpec.bodyToMono(KakaoUserInfoResponse.class))
+                .thenReturn(Mono.just(new KakaoUserInfoResponse(null, null)));
 
         assertThatThrownBy(() -> client.getKakaoUserInfo("access-token"))
                 .isInstanceOf(GeneralException.class)
@@ -140,6 +164,25 @@ class OAuthKakaoClientTest {
                 .isInstanceOf(GeneralException.class)
                 .extracting("errorStatus")
                 .isEqualTo(ErrorStatus.KAKAO_UNLINK_FAILED);
+    }
+
+    @Test
+    void handleErrorReturnsKakaoTokenRequestFailure() {
+        OAuthKakaoClient client = new OAuthKakaoClient(mock(WebClient.class), kakaoProperties());
+
+        @SuppressWarnings("unchecked")
+        Mono<Throwable> result = ReflectionTestUtils.invokeMethod(
+                client,
+                "handleError",
+                HttpStatus.BAD_REQUEST,
+                "error-body",
+                "사용자 정보 조회"
+        );
+
+        assertThatThrownBy(result::block)
+                .isInstanceOf(GeneralException.class)
+                .extracting("errorStatus")
+                .isEqualTo(ErrorStatus.KAKAO_TOKEN_REQUEST_FAILED);
     }
 
     private KakaoProperties kakaoProperties() {
